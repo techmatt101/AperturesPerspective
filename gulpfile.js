d455a6afa22fe38cff6b58101110fa5910073fec
techmatt101 <matt@kempmail.co.uk>
2014-11-15 22:07:21 +0000

    build now supports templates from the template folder

--- gulpfile.js
+++ gulpfile.js
@@ -7,37 +7,70 @@ var wrap = require('gulp-wrap');
 var declare = require('gulp-declare');
 var concat = require('gulp-concat');
 
-gulp.task('default', ['styles', 'scripts', 'templates']);
 
-gulp.task('scripts', function() {
-    var tsResult = gulp.src('core/*.ts')
-        .pipe(sourcemaps.init())
-        .pipe(ts({
-            sortOutput : true,
-            target: 'ES5'
-        }));
-
-    return tsResult.js
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/js'));
-});
-
-gulp.task('styles', function() {
-    return gulp.src('styles/*.less')
-        .pipe(sourcemaps.init())
-        .pipe(less())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/css'));
-});
-
-gulp.task('templates', function(){
-    return gulp.src('views/*.hbs')
+function buildTemplates(task) {
+    return task
         .pipe(handlebars())
         .pipe(wrap('Handlebars.template(<%= contents %>)'))
         .pipe(declare({
             namespace: 'hbs',
-            noRedeclare: true // Avoid duplicate declarations
-        }))
+            noRedeclare: true
+        }));
+}
+
+function buildStyles(task) {
+    return task
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write());
+}
+
+function buildScripts(task) {
+    var tsResult = task
+        .pipe(sourcemaps.init())
+        .pipe(ts({
+            sortOutput: true,
+            target: 'ES5'
+        }));
+
+    return tsResult.js
+        .pipe(sourcemaps.write());
+}
+
+gulp.task('default', ['views', 'templates', 'styles', 'scripts']);
+gulp.task('dev', ['scripts', 'styles', 'views', 'templates']);
+gulp.task('templates', ['template-markup', 'template-styles', 'template-scripts']);
+
+gulp.task('views', function () {
+    return buildTemplates(gulp.src('views/*.hbs'))
+        .pipe(concat('views.js'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('styles', function () {
+    return buildStyles(gulp.src('styles/*.less'))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('scripts', function () {
+    return buildScripts(gulp.src('core/*.ts'))
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('template-markup', function () {
+    return buildTemplates(gulp.src('templates/*/template.hbs'))
         .pipe(concat('templates.js'))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('template-styles', function () {
+    return buildStyles(gulp.src('templates/*/style.less'))
+        .pipe(concat('templates.css'))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('template-scripts', function () {
+    return buildScripts(gulp.src('templates/*/script.ts'))
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest('build/js'));
 });
